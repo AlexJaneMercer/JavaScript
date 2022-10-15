3b5ab05c16e7fc611422495bba5c1467d84be165
AlexJaneMercer <a.andrey2001.a@mail.ru>
2022-10-15 16:06:47 +0300

    Added some buttons and elements for other figures

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let res = prompt("Введите размер стороны куба: ");
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+
+btn1.addEventListener('click', ()=> {
+    let res = prompt("Введите размер стороны куба: ");
 
 function cubeVolume(a){
     let result = a*a*a;
     return result;
 }
-
 
 function cubeSquare(a) {
     let result = 6 * (cubeVolume(a) * cubeVolume(a));
@@ -16,4 +23,6 @@ function cubeDiagonal(a){
     return result;
 }
 
-alert("Объём куба = " + cubeVolume(res) + " Площадь куба = " + cubeSquare(res) + "Диагональ куба = " + cubeDiagonal(res));
+alert(" Объём куба = " + cubeVolume(res) + "\n Площадь куба = " + cubeSquare(res) + "\n Диагональ куба = " + cubeDiagonal(res));
+})
+
